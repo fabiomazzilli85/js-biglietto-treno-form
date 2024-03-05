1cd8fd9d5d272bab47c9ace8849bd2d6cd50e4cd
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-03-05 16:37:06 +0100

    Replicato JS scritto ieri.

--- js/app.js
+++ js/app.js
@@ -1,20 +1,33 @@
-function verificaPrezzo() {
+// Informazioni preliminari del passeggero: età e chilometri da percorrere.
 
-    const distanza = parseFloat(document.getElementById("kmViaggio").value);
-    const etaPasseggero = parseInt(document.getElementById("etaPasseggero").value);    
+let distanza = document.getElementById("kmViaggio").value; //number
+let eta = document.getElementById("etaPasseggero").value; //number
 
 
-    const costoBase = 0.21 * distanza; // Number
+// Calcolo del costo del biglietto del treno
+let costoBiglietto = kmViaggio*0.21; //number
 
-// Se il passeggero ha meno di 18 anni, pagherà un biglietto con lo sconto del 20%. Se il passeggero ha più di 65 anni pagherà un biglietto con lo sconto del 40%. 
-// Chi non rietra in queste due categorie, pagherà il biglietto standard. 
 
-    let costoBiglietto;
-    if (etaPasseggero < 18) {
-        costoBiglietto = costoBase * 0.8;
-    } else if (etaPasseggero > 65) {
-        costoBiglietto = costoBase * 0.6; 
-    } else {
-        costoBiglietto = costoBase;
-    }
+// Calcolo del costo del bilgietto in base all'eta
+
+let bigliettoMinori = costoBiglietto * 0.8; //number 
+// Costo del biglietto per i  passeggeri con età inferiore ai 18 anni
+
+let bigliettoAnziani = costoBiglietto * 0.6; //number
+// Costo del biglietto per i  passeggeri con età superiore ai 65 anni
+
+let bigliettoStandard = costoBiglietto; //number
+
+
+// Console Log
+if (etaPasseggero <18){
+    console.log("Il costo del biglietto per i minori è: " + bigliettoMinori.toFixed(2) + " euro");
+}
+
+else if (etaPasseggero >65){
+    console.log("Il costo del biglietto per gli anziani è: " + bigliettoAnziani.toFixed(2) + " euro");
+}
+
+else {
+    console.log("Il costo del biglietto è: " + bigliettoStandard.toFixed(2) + " euro");
 }
